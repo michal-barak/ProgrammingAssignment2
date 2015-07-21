d0d32c18346489df368f8a173269fb6c46f76685
Michal Barak <michal.barak@gmail.com>
2015-07-21 12:26:11 +0300

    2nd assignment R course
    create the function for cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an implementation of finding invers of a matrix and saving it 
+## to avoid unnecessary redundant calculations
 
-## Write a short comment describing this function
+## makeCachMAtrix creates a mtrix with a list of commands that get and set
+## the matrix, and get and set the invers
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invrs <- NULL
+    set <- function (y) {
+        x <<- y
+        invrs <<-NULL
+    }
+    
+    get <- function () x
+    setinvers <- function(invers){        
+        invrs <<- invers        
+    }
+    getinvers <- function() {        
+        invrs
+    }
+    list(set = set, get = get,
+         setinvers = setinvers,
+         getinvers = getinvers)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the invers of the input matrix, if cashed, from the 
+## cashed value, else - or if the matrix was changed, it is calculated again
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invrs <- x$getinvers()        
+        if (!is.null(invrs)){
+            print("from cashed data")
+            return (invrs)
+        }
+        
+        data <- x$get()
+        invrs <- solve(data, ...)
+        x$setinvers(invrs)
+        invrs        
 }
